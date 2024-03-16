2bfb787b805d4f57bfcfe50192e6cbd4e98148dd
HIMANSHU KUMAR SAH <141276345+himanshusah90@users.noreply.github.com>
2024-03-17 02:53:52 +0530

    Add files via upload
    Here you add the photos inside another folder called "assets" then it creates another folder with the resized images.

---
+++ Imageresizer.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import os
+
+def resize(im, new_width):
+    width, height = im.size
+    ratio = height/width
+    new_height = int(ratio*new_width)
+    resized_image = im.resize((new_width,new_height))
+    return resized_image
+
+files = os.listdir("assets")
+extensions = ['jpg','png','jpeg']
+for file in files:
+    ext = file.split('.')[-1]
+    if ext in extensions:
+        im = Image.open('assets/'+file)
+        im_resized = resize(im, 600)
+        new_dir = 'resized_images'
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
+        file = f'{new_dir}/{file}.jpg'
+        im_resized.save(file, 'png', quality=100)
+
